c6d8bdddffb30cec4b37d5713e91da9c865d9beb
stskri <177446189+stskri@users.noreply.github.com>
2024-11-08 07:54:56 +0000

    Add my_page

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,15 @@
 class Public::UsersController < ApplicationController
+  def show
+  end
+
+  def my_page
+    @user = current_user
+    @post = Post.where(user: current_user)
+  end
+
+  def edit    
+  end
+
+  def update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   scope module: :public do
     root :to => "homes#top"
     get "about" => "homes#about"
+    get "my_page" => "users#my_page"
     resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+    resources :user, only: [:show, :edit, :update]
   end
 end
