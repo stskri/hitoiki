3790e1f4859d82d84cd47fe1003bb00d1110211a
stskri <177446189+stskri@users.noreply.github.com>
2024-11-08 06:30:08 +0000

    Add Post.crud

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,19 +19,29 @@ class Public::PostsController < ApplicationController
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post.id), notice: '投稿を編集しました'
+    else
+      redirect_to edit_post_path(post.id), alert: '投稿の編集に失敗しました'
+    end
   end
 
   def destroy
-
+    post = Post.find(params[:id])
+    if post.destroy
+      redirect_to posts_path, notice: '投稿を削除しました'
+    else
+      redirect_to post_path(post.id), alert: '投稿の削除に失敗しました'
+    end
   end
 
 
