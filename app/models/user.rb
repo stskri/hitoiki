f31b9e4101c31a8eedf6f5697946889b51de15cf
stskri <177446189+stskri@users.noreply.github.com>
2024-11-10 05:22:18 +0000

    実装フェーズ1完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@ class User < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :rooms, dependent: :destroy
   has_many :messages, dependent: :destroy
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_fill: [width, height]).processed
+  end
 end
