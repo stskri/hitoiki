360d80c745fa6f9ed68f156e09cf8deb59c2dc97
stskri <177446189+stskri@users.noreply.github.com>
2024-11-14 04:55:33 +0000

    Add admin_side-bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seedを作成します。"
+
+# adminの設定
+Admin.find_or_create_by(id: 1) do |admin|
+  admin.email = 'a@a'
+  admin.password = 'aaaaaa'
+  admin.password_confirmation = 'aaaaaa'
+end
+# adminの設定ここまで
+
+
+puts "seedの作成が完了しました。"
