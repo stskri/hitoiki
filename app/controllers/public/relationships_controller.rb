ada26b0680274d4d32306a21e18b5c6b4e1a21e2
stskri <177446189+stskri@users.noreply.github.com>
2024-11-16 13:13:47 +0000

    Add fearure/relationships

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -9,7 +9,7 @@ class Public::RelationshipsController < ApplicationController
 
   def destroy
     user = User.find(params[:user_id])
-    current_user.usfollow(user)
+    current_user.unfollow(user)
     redirect_to request.referer
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
     end
     resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
     end
