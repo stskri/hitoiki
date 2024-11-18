afb67444bd822ae51eb603f2b2210072f5170063
stskri <177446189+stskri@users.noreply.github.com>
2024-11-18 09:11:12 +0000

    Add ajax to relationships/btn

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,15 +2,13 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to request.referer, notice: 'フォローしました'
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to request.referer, notice: 'フォローを解除しました'
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end
 
   def followings
