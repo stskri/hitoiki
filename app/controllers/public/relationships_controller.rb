6542bb0b9d9f42c31e07cf166606711f2248d69c
stskri <177446189+stskri@users.noreply.github.com>
2024-11-18 04:30:40 +0000

    Edit follow-related layout

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -15,11 +15,11 @@ class Public::RelationshipsController < ApplicationController
 
   def followings
     @user = User.find(params[:user_id])
-    @followings = user.followings
+    @followings = @user.followings
   end
 
   def followers
     @user = User.find(params[:user_id])
-    @followers = user.followers
+    @followers = @user.followers
   end
 end
