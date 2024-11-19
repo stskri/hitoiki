788c8014eb0dff970e25db9e6a3aab28f3fec228
stskri <177446189+stskri@users.noreply.github.com>
2024-11-19 09:18:52 +0000

    Add rooms/message_controller

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,3 +1,37 @@
 class Public::MessagesController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    @room = Room.find(params[:id])
+    unless @room.users.include?(current_user)
+      redirect_to rooms_path, alert: "メッセージの送信に失敗しました" and return
+    end
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    @message.room.id = @room.id
+    if @message.save
+      redirect_to request.referer, notice: 'メッセージを送信しました'
+    else
+      redirect_to request.referer, alert: 'メッセージの送信に失敗しました'
+    end
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    if message.user == current_user
+      if message.destroy
+        redirect_to request.referer, notice: 'メッセージを削除しました'
+      else
+        redirect_to request.referer, alert: 'メッセージの削除に失敗しました'
+      end
+    else
+      redirect_to request.referer, alert: "無効なアクセスです"
+    end
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:message)
+  end
 end
--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,3 +1,36 @@
 class Public::RoomsController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def create
+    @room = Room.create
+    Entry.create(user_id: current_user.id, room_id: @room.id)
+    Entry.create(user_id: params[:id], room_id: @room.id)
+    redirect_to room_path(@room)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    unless @rooms.users.include?(current_user)
+      redirect_to rooms_path, alert: "無効なアクセスです" and return
+    end
+    @messages = @rooms.messages
+    @message = Message.new
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    if room.users.include(current_user)
+      if room.destroy
+        redirect_to rooms_path, notice: "削除しました"
+      else
+        redirect_to rooms_path, alert: "ルームの削除に失敗しました"
+      end
+    else
+      redirect_to rooms_path, alert: "無効なアクセスです"
+    end
+  end
 end
