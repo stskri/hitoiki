1ec14a2937476c588f360aac3629611922df705e
stskri <177446189+stskri@users.noreply.github.com>
2024-11-19 13:43:54 +0000

    Add user.name to message-room-page

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -6,13 +6,19 @@ class Public::RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(user_1_id: params[:room][:user_1_id], user_2_id: params[:room][:user_2_id])
-    if @room.save
-      Entry.create(user_id: @room.user_1_id, room_id: @room.id)
-      Entry.create(user_id: @room.user_2_id, room_id: @room.id)
-      redirect_to room_path(@room), notice: "メッセージルームが作成されました"
+    existing_room = Room.find_by(user_1_id: params[:room][:user_1_id], user_2_id: params[:room][:user_2_id]) ||
+                    Room.find_by(user_1_id: params[:room][:user_2_id], user_2_id: params[:room][:user_1_id])
+    if existing_room != nil
+      redirect_to room_path(existing_room)
     else
-      redirect_to request.referer, alert: "メッセージルームの作成に失敗しました"
+      @room = Room.new(user_1_id: params[:room][:user_1_id], user_2_id: params[:room][:user_2_id])
+      if @room.save
+        Entry.create(user_id: @room.user_1_id, room_id: @room.id)
+        Entry.create(user_id: @room.user_2_id, room_id: @room.id)
+        redirect_to room_path(@room), notice: "メッセージルームが作成されました"
+      else
+        redirect_to request.referer, alert: "メッセージルームの作成に失敗しました"
+      end
     end
   end
 
