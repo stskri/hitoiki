900d9bc5f2f217ac41f224ebf3fe07daf176f76d
stskri <177446189+stskri@users.noreply.github.com>
2024-11-25 02:18:26 +0000

    Fix post_emotions#create

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :post_emotions, dependent: :destroy
+  has_many :emotions, through: :post_emotions
   has_many :post_comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
