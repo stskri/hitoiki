022d4417573bb0e06a1ed6d2dbe9ca1486993bc7
stskri <177446189+stskri@users.noreply.github.com>
2024-11-27 10:27:16 +0000

    Add guest-login

---
+++ app/controllers/public/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+module Public
+  class Users::SessionsController < Devise::SessionsController
+    def guest_sign_in
+      user = User.guest
+      sign_in user
+      redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    end
+  end
+end
