9e9b40b6750a81850ef16c87f58c9b4dd520c1e2
stskri <177446189+stskri@users.noreply.github.com>
2024-11-28 15:43:34 +0000

    Add feature/withdraw

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_suspension?, only: [:create]
 
 
   def after_sign_in_path_for(resource)
@@ -11,6 +12,7 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
+
 
   # GET /resource/sign_in
   # def new
@@ -27,7 +29,29 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def user_suspension?
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user
+      if @user.valid_password?(params[:user][:password]) && @user.active_for_authentication?
+        # 正常なログイン処理（ユーザーが存在し、パスワードも正しく、アカウントが有効な場合）
+      elsif @user.valid_password?(params[:user][:password]) && !@user.active_for_authentication?
+        # アカウントが無効（:is_activeがfalse）でパスワードが正しい場合
+        flash[:alert] = "アカウントは管理者によって停止されています"
+        redirect_to root_path and return
+      else
+        # パスワードが間違っている場合
+        flash[:alert] = "パスワードが間違っています"
+        redirect_to root_path and return
+      end
+    else
+      # メールアドレスが見つからない場合
+      flash[:alert] = "該当するユーザーが見つかりません"
+      redirect_to root_path and return
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
