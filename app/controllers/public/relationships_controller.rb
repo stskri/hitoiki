e66e87b1296a574b60b5a93e0f6c441c7541f0ee
stskri <177446189+stskri@users.noreply.github.com>
2024-12-01 14:32:26 +0000

    Add more_followings_btn to public/relationships#followings

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -15,7 +15,7 @@ class Public::RelationshipsController < ApplicationController
 
   def followings
     @user = User.find(params[:user_id])
-    @followings = @user.followings.page(params[:page]).per(1)
+    @followings = @user.followings.page(params[:page]).per(25)
   end
 
   def followers
