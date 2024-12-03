16f5555395d3284035e5a509238020a48edf6bab
stskri <177446189+stskri@users.noreply.github.com>
2024-12-03 12:16:51 +0000

    Add i18n to enum

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,4 +3,9 @@ class Inquiry < ApplicationRecord
 
   validates :body, presence: true
   validates :genre, presence: true
+
+  enum genre: {account: 0, feature_request: 1, bug_report: 2, other: 3}
+
+  validates :body, length: { maximum: 1000 }, presence: true
+  validates :genre, presence: true
 end
