9a7ff789b262a24f45ca7fa8b83951deefcc22ef
stskri <177446189+stskri@users.noreply.github.com>
2025-01-11 06:38:33 +0000

    Add notifications_path to user-left-bar

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -12,4 +12,13 @@ class Public::NotificationsController < ApplicationController
                     .order(created_at: :desc) # 新着が上になるよう指定
                     .page(params[:page]).per(25) # 追加で25件を取得
   end
+
+  def index
+    @notifications = current_user.passive_notifications
+                    .where.not(visitor_id: current_user.id) # 自分から自分へのものは除く
+                    .where(is_active: true) # アクティブの通知のみに絞り込み
+                    .includes(:visitor, :post, :post_comment, :message)
+                    .order(created_at: :desc) # 新着が上になるよう指定
+                    .page(params[:page]).per(25) # サイドバー用に最新25件だけ取得
+  end
 end
